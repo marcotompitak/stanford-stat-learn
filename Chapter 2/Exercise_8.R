63195601b4ccdbfbe758ef23c92b6b9a34b6478b
Marco Tompitak <marcotompitak@gmail.com>
2017-04-18 12:37:02 +0200

    Cleaned up directory structure and updated Exercise 8

--- Chapter 2/Exercise_8.R
+++ Chapter 2/Exercise_8.R
@@ -1,5 +1,5 @@
 # Reading in the data
-College=read.csv("College.csv")
+College=read.csv("../Data/College.csv")
 
 # Converting the first column into row names
 rownames(College)=College[,1]
@@ -7,11 +7,11 @@ College=College[,-1]
 
 # Producing a matrix of pairwise plots, saved to Pairs.png
 pairs(College)
-dev.copy(png,'Pairs.png', width = 40, height = 40, units = 'cm', res = 300); dev.off()
+dev.copy(png,'./Output/Ex8_Pairs.png', width = 40, height = 40, units = 'cm', res = 300); dev.off()
 
 # Creating boxplots of Outstate vs Private
 plot(Outstate~Private,College)
-dev.copy(png,'Boxplot.png', width = 10, height = 10, units = 'cm', res = 300); dev.off()
+dev.copy(png,'./Output/Ex8_Boxplot.png', width = 10, height = 10, units = 'cm', res = 300); dev.off()
 
 # Define Elite category
 Elite = rep("No",nrow(College))
@@ -21,20 +21,13 @@ College=data.frame(College,Elite)
 
 # Boxplot of Outstate vs Elite
 plot(Outstate~Elite,College)
-dev.copy(png,'Boxplot2.png', width = 10, height = 10, units = 'cm', res = 300); dev.off()
+dev.copy(png,'./Output/Ex8_Boxplot2.png', width = 10, height = 10, units = 'cm', res = 300); dev.off()
 
 # Making histograms
 par(mfrow=c(2,2))
-hist(College$Enroll,100,xlab="Enroll",main="Histogram of Enroll")
-hist(College$Apps,100,xlab="Apps",main="Histogram of Apps")
-hist(College$Accept,100,xlab="Acept",main="Histogram of Accept")
-hist(College$PhD,100,xlab="PhD",main="PhD")
-dev.copy(png,'Histograms.png', width = 15, height = 15, units = 'cm', res = 300); dev.off()
+hist(College$Enroll,50,xlab="Enroll",main="Histogram of Enroll")
+hist(College$Apps,50,xlab="Apps",main="Histogram of Apps")
+hist(College$Accept,50,xlab="Acept",main="Histogram of Accept")
+hist(College$PhD,50,xlab="PhD",main="PhD")
+dev.copy(png,'./Output/Ex8_Histograms.png', width = 15, height = 15, units = 'cm', res = 300); dev.off()
 
-# Fitting a linear model for the graduation rate
-fit_lin = lm(Grad.Rate~.,College)
-summary(fit_lin)
-
-# Many variables do not seem significantly predictive
-fit_lin = update(fit_lin,~.-Accept-Enroll-Top10perc-F.Undergrad-Books-PhD-Terminal-S.F.Ratio-EliteYes)
-summary(fit_lin)
